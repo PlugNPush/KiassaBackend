169d9051c37886afa17554c89972ea9ab99f8909
Fefoler01 <72406859+Fefoler01@users.noreply.github.com>
2022-04-27 00:32:30 +0200

    fix registration

--- v1/registration.php
+++ v1/registration.php
@@ -23,12 +23,12 @@ if ($method == 'POST') {
     }
   }
   if (!empty($data['plainpassword'])){
-    if(!preg_match("^\S*(?=\S{8,})(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])(?=\S*[\W])\S*$", $data['plainpassword'])){
+    if(!preg_match("/^\S*(?=\S{8,})(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])(?=\S*[\W])\S*$/", $data['plainpassword'])){
       $errors[]='password_not_conform';
     }
   }
   if (!empty($data['telephone'])){
-    if(!preg_match("^(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})$", $data['telephone'])){
+    if(!preg_match("/^(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})$/", $data['telephone'])){
       $errors[]='invalid_telephone';
     }
   }
