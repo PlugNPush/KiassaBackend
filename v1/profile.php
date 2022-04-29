7815205650145d9f38b29c0635e201b1c94bdcff
LonXag <73125125+LonXag@users.noreply.github.com>
2022-04-29 17:57:01 +0200

    test2

--- v1/profile.php
+++ v1/profile.php
@@ -3,8 +3,82 @@
 require_once '../config/core.php';
 
 if ($method == 'PUT') {
+  # Modifier les données
 
- http_response_code(200);
+  # test si le nom est valide
+  $errors=array();
+  if (empty($data['id'])){
+    $errors[]='missing_id';
+  } else {
+    $req = $db->prepare('SELECT * FROM users WHERE id = ?;');
+    $req->execute(array($data['id']));
+    $test = $req->fetch();
+
+    if (!$test) {
+      $errors[]='invalid_id';
+    }
+  }
+
+  if (!empty($errors)){
+    http_response_code(400); # bad request
+
+    echo json_encode(array(
+      "status" => false,
+      "description" => $errors,
+      "returntosender"=>$data
+    ));
+
+    } else {
+
+      if (!empty($data['name'])){
+
+        if($data['name'])!=NULL){
+
+          $req = $db->prepare('UPDATE users SET name = ? WHERE id = ?;');
+          $test = $req->execute(array($data['name'], $data['id']));
+
+          if ($test){ # message bien modifié
+
+            http_response_code(200); # Ok
+
+            echo json_encode(array(
+              "status" => true,
+              "description" => array("success name change"),
+              "data" => $test
+            ));
+
+          } else {
+            http_response_code(502); # bad gateway
+
+            echo json_encode(array(
+              "status" => false,
+              "description" => array("internal_error -> name change"),
+              "returntosender" => $data
+            ));
+          }
+        } else{
+
+          http_response_code(400); # bad request
+
+          echo json_encode(array(
+            "status" => false,
+            "description" => array("bad request -> name can't be NULL"),
+            "returntosender" => $data
+          ));
+        }
+      }
+
+      if (!empty($data['name']) AND !empty($data['telephone']) AND !empty($data['photo']) AND !empty($data['address']) AND !empty($data['password'])){
+
+        http_response_code(400); # bad request
+
+        echo json_encode(array(
+          "status" => false,
+          "description" => array("internal_error -> no data"),
+          "returntosender" => $data
+        ));
+      }
+  }
 
 } else {
 
