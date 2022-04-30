dd6a48e0335bd27724bd33fb4f42cc0f64a12c46
LonXag <73125125+LonXag@users.noreply.github.com>
2022-04-30 02:15:39 +0200

    test11

--- v1/profile.php
+++ v1/profile.php
@@ -13,9 +13,49 @@ if ($method == 'PUT') {
     $req = $db->prepare('SELECT * FROM users WHERE id = ?;');
     $req->execute(array($data['id']));
     $test = $req->fetch();
+
+    if (!$test) {
+      $errors[]='invalid_id';
+    }
   }
 
+  if (!empty($errors)){
+    http_response_code(400); # bad request
 
+    echo json_encode(array(
+      "status" => false,
+      "description" => $errors,
+      "returntosender"=>$data
+    ));
+
+    } else {
+
+      if (!empty($data['name'])){
+
+          $req = $db->prepare('UPDATE users SET name = ? WHERE id = ?;');
+          $test = $req->execute(array($data['name'], $data['id']));
+
+          if ($test){ # message bien modifié
+
+            http_response_code(200); # Ok
+
+            echo json_encode(array(
+              "status" => true,
+              "description" => array("success name change"),
+              "data" => $test
+            ));
+
+          } else {
+            http_response_code(502); # bad gateway
+
+            echo json_encode(array(
+              "status" => false,
+              "description" => array("internal_error -> name change"),
+              "returntosender" => $data
+            ));
+          }
+      }
+  }
 
 } else {
 
