17b920348f03ba54866e6fa27a53f6b62914b9b0
Fefoler01 <72406859+Fefoler01@users.noreply.github.com>
2022-05-01 20:21:03 +0200

    fix logout

--- v1/logout.php
+++ v1/logout.php
@@ -4,61 +4,73 @@ require_once '../config/core.php';
 
 if ($method == 'DELETE') {
 
-  // Return alert
+  if ($connected['status'] == true){
 
-  # test si les données sont vides
-  $errors=array();
-  if (empty($data['token'])){
-    $errors[]='missing_token';
-  } else {
-    $req = $db->prepare('SELECT * FROM tokens WHERE token = ?;');
-    $req->execute(array($data['token']));
-    $test = $req->fetch();
-    if (!$test){
-      $errors[]='token_not_exists';
+    // Return alert
+
+    # test si les données sont vides
+    $errors=array();
+    if (empty($connected['tokendata']['token'])){
+      $errors[]='missing_token';
+    } else {
+      $req = $db->prepare('SELECT * FROM tokens WHERE token = ?;');
+      $req->execute(array($connected['tokendata']['token']));
+      $test = $req->fetch();
+      if (!$test){
+        $errors[]='token_not_exists';
+      }
     }
-  }
 
 
-  if (!empty($errors)){
-    http_response_code(400); # bad request
+    if (!empty($errors)){
+      http_response_code(400); # bad request
 
-    echo json_encode(array(
-      "status" => false,
-      "description" => $errors,
-      "returntosender"=>$data
-    ));
+      echo json_encode(array(
+        "status" => false,
+        "description" => $errors,
+        "returntosender"=>$data
+      ));
 
-    exit();
-  }
+      exit();
+    }
 
-  # supprimer token de la db
-
-
-  $req2 = $db->prepare('DELETE FROM tokens WHERE token = ?;');
-  $req2->execute(array($data['token']));
-
-  $req3 = $db->prepare('SELECT * FROM tokens WHERE token = ?;');
-  $req3->execute(array($data['token']));
-  $test3 = $req3->fetch();
+    # supprimer token de la db
 
 
-  if (!$test3){ # token bien supprimé
+    $req2 = $db->prepare('DELETE FROM tokens WHERE token = ?;');
+    $req2->execute(array($connected['tokendata']['token']));
 
-    http_response_code(200); # Ok
+    $req3 = $db->prepare('SELECT * FROM tokens WHERE token = ?;');
+    $req3->execute(array($connected['tokendata']['token']));
+    $test3 = $req3->fetch();
 
-    echo json_encode(array(
-      "status" => true,
-      "description" => array("success")
-    ));
 
+    if (!$test3){ # token bien supprimé
+
+      http_response_code(200); # Ok
+
+      echo json_encode(array(
+        "status" => true,
+        "description" => array("success")
+      ));
+
+    } else {
+      http_response_code(502); # bad gateway
+
+      echo json_encode(array(
+        "status" => false,
+        "description" => array("internal_error"),
+        "returntosender" => $data
+      ));
+    }
   } else {
-    http_response_code(502); # bad gateway
+    http_response_code(403); # forbiden
 
     echo json_encode(array(
       "status" => false,
-      "description" => array("internal_error"),
-      "returntosender" => $data
+      "description" => array("invalid_token"),
+      "returntosender" => $data,
+      "returnheaders" => $headers
     ));
   }
 } else {
@@ -73,6 +85,5 @@ if ($method == 'DELETE') {
     "returnmethod" => $method,
     "returntosender" => $data
   ));
-
 }
 ?>
