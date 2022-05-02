491b1256a0fef8d5ca7170a03a65109657a97efa
Fefoler01 <72406859+Fefoler01@users.noreply.github.com>
2022-05-02 19:35:14 +0200

    oups

--- v1/profile.php
+++ v1/profile.php
@@ -17,7 +17,7 @@ if ($method == 'PATCH') {
     }
 
     if (!isset($data['telephone'])){
-      $data['telephone']=$connected['data']['telephone']
+      $data['telephone']=$connected['data']['telephone'];
     } else if (!empty($data['telephone'])){
       if(preg_match("/^(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})$/", $data['telephone']) === 0){
         $errors[]='invalid_telephone';
